d5c7484c874fece857f904016f336eff40ce6492
Oliver Vitale <ovitale18@gmail.com>
2023-11-01 14:54:09 -0400

    Installed needed packages

---
+++ index.js
@@ -0,0 +1,5 @@
+//Including packages needed for this application 
+const fs = require("fs");
+const inquirer = require ("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
