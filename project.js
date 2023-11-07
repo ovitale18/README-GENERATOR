eb8f149f259d73276d86c158c5ea541d85d8b0da
Oliver Vitale <ovitale18@gmail.com>
2023-11-07 12:13:33 -0500

    Wrote function to read across the slot in rows as apposed to vertical columns. 'transpose'

--- project.js
+++ project.js
@@ -79,8 +79,9 @@ const spin = () => {
        }
     }
 
-    const reels = [[], [], []];
+    const reels = [];
     for (let i = 0; i < COLS; i++) {
+        reels.push([]);
         const reelSymbols = [...symbols];
         for (let j = 0; j < ROWS; j++) {
             const randomIndex = Math.floor(Math.random() * reelSymbols.length)
@@ -93,10 +94,25 @@ const spin = () => {
     return reels;
 };
 
+//Function to read across columns as rows
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i< ROWS; i++) {
+        rows.push([]);
+        for (let j = 0; j < COLS; j++) {
+            rows[i].push(reels[j][i])
+        }
+    }
+
+    return rows
+};
+
 
 //Calling functions
-const reels = spin();
-console.log(reels);
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+
