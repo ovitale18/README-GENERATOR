ea2cd7801ece39adf5c30484f5610b729266a25c
Oliver Vitale <ovitale18@gmail.com>
2023-11-07 10:09:24 -0500

    Wrote function to take bet amount

--- project.js
+++ project.js
@@ -14,8 +14,8 @@ const deposit = () => {
     const depositAmount = prompt("Enter a deposit amount: ");
     const numberDepositAmount = parseFloat(depositAmount);
 
-    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0){
-        console.log("Invalid deposit amount, try again.")
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+        console.log("Invalid deposit amount, please try again.")
     } else {
         return numberDepositAmount;
     }
@@ -28,14 +28,29 @@ const getNumberOfLines = () => {
         const lines = prompt("Enter the number of lines you'd like to bet on (1-3): ");
         const numberOfLines = parseFloat(lines);
     
-        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
-            console.log("Invalid number of lines, try again.")
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, please try again.")
         } else {
             return numberOfLines;
         }
      }
 };
 
+//Step 3: Get bet amount
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter your total bet per line: ");
+        const numberBet = parseFloat(bet);
+    
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+            console.log("Invalid bet, please try again.")
+        } else {
+            return numberBet;
+        }
+     }
+}
+
 //Calling functions
-const depositAmount = deposit();
+let balance = deposit();
 const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
